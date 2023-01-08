6a4b7aea0ef1d1b526b3e05ec10461fde87f7892
nbminh04 <nbminh04@gmail.com>
2023-01-08 15:58:34 +0700

    Lesson 02

---
+++ Lesson_02/index.js
@@ -0,0 +1,35 @@
+
+var jsb08 = ['Long', 'Khánh','Thành','Quyền','Nguyên','Ngọc Anh']
+
+var jsb05 = ['Huy', 'Linh']
+
+// console.log(jsb08);
+
+// console.log(jsb08.length)
+
+// jsb08[1] = 'Linh' //Gán lại giá trị của 1 phần tử trong mảng
+
+//In tất tất cả thông tin của mảng jsb08
+
+// for(var i = 0;i<jsb08.length;i++){
+//     console.log(jsb08[i])
+// }
+
+// for(var i of jsb08){
+//     console.log(i);
+// }
+
+// console.log(jsb08.shift());
+
+
+console.log(jsb08);
+console.log(jsb05);
+
+var jsa08 =  jsb08.concat(jsb05)
+
+console.log(jsa08);
+
+console.log(jsa08.length);
+
+
+
